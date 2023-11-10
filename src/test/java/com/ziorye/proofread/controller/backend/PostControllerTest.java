a22135238a50b05b07ecd3db6b087eac9449000c
ziorye <ziorye@gmail.com>
2023-11-10 15:36:12 +0800

    test storeWithCoverImage

--- src/test/java/com/ziorye/proofread/controller/backend/PostControllerTest.java
+++ src/test/java/com/ziorye/proofread/controller/backend/PostControllerTest.java
@@ -9,11 +9,15 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.File;
+import java.nio.file.Files;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -65,6 +69,33 @@ class PostControllerTest extends WithMockUserForAdminBaseTest {
 
         Optional<Post> po = postRepository.findFirstByTitle(title);
         Assertions.assertTrue(po.isPresent());
+
+        postRepository.delete(po.get());
+    }
+
+    @Test
+    void storeWithCoverImage(@Autowired PostRepository postRepository, @Autowired Environment env) throws Exception {
+        String title = "title-" + UUID.randomUUID();
+        MockMultipartFile coverFile = new MockMultipartFile("coverFile", "cover.png", MediaType.IMAGE_PNG_VALUE, new byte[] { 1, 2, 3 });
+        mvc.perform(MockMvcRequestBuilders
+                        .multipart("/backend/post/store")
+                        //.contentType(MediaType.MULTIPART_FORM_DATA)
+                        .file(coverFile)
+                        .param("id", "")
+                        .param("user_id", "1")
+                        .param("title", title)
+                        .param("content", "content-" + UUID.randomUUID())
+                )
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/backend/posts"))
+        ;
+
+        Optional<Post> po = postRepository.findFirstByTitle(title);
+        Assertions.assertTrue(po.isPresent());
+
+        String cover = po.get().getCover();
+        File coverOnDisk = new File(env.getProperty("custom.upload.base-path") + File.separator + cover);
+        Assertions.assertTrue(Files.exists(coverOnDisk.toPath()));
+        Assertions.assertTrue(coverOnDisk.delete());
 
         postRepository.delete(po.get());
     }
