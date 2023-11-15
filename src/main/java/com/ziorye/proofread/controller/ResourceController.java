79cc0d1caeef54ad5eb297c99a93859d662936c8
ziorye <ziorye@gmail.com>
2023-11-15 18:02:17 +0800

    download resource

--- src/main/java/com/ziorye/proofread/controller/ResourceController.java
+++ src/main/java/com/ziorye/proofread/controller/ResourceController.java
@@ -2,9 +2,18 @@ package com.ziorye.proofread.controller;
 
 import com.ziorye.proofread.entity.Post;
 import com.ziorye.proofread.service.PostService;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +22,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Optional;
 
+@Slf4j
 @Controller
 @RequestMapping("/resources")
 public class ResourceController {
@@ -44,5 +57,48 @@ public class ResourceController {
 
         model.addAttribute("post", optionalPost.get());
         return "resource/show";
+    }
+
+    @GetMapping("/download/{id}")
+    @PreAuthorize("isAuthenticated()")
+    void download(@PathVariable Long id, HttpServletResponse response, @AuthenticationPrincipal UserDetails user) throws IOException {
+        Post post = postService.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        FileInputStream fis = getFileInputStream(post);
+
+        ServletOutputStream sos = response.getOutputStream();
+        response.setHeader("Content-Disposition", "attachment;filename=resource-" + id + "." + FilenameUtils.getExtension(post.getAttachment()));
+        IOUtils.copy(fis, sos);
+        fis.close();
+
+        log.info(user.getUsername() + " downloaded resource[{}]", id);
+    }
+
+    @Value("${custom.upload.base-path}")
+    String uploadBasePath;
+    @Value("${custom.upload.chunked-dir-under-base-path}")
+    String chunkedDirUnderBasePath;
+    private FileInputStream getFileInputStream(Post post) {
+        String attachment = post.getAttachment();
+
+        if (!post.isStatus()
+                || !"resource".equals(post.getType())
+                || "".equals(attachment)
+                || attachment == null
+        ) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        String path = "src/main/resources/static" + attachment;
+        if (attachment.startsWith("/" + chunkedDirUnderBasePath)) {
+            path = uploadBasePath + attachment;
+        }
+        FileInputStream fis;
+        try {
+            fis = new FileInputStream(path);
+        } catch (FileNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return fis;
     }
 }
--- src/test/java/com/ziorye/proofread/controller/ResourceControllerTest.java
+++ src/test/java/com/ziorye/proofread/controller/ResourceControllerTest.java
@@ -3,10 +3,12 @@ package com.ziorye.proofread.controller;
 import com.ziorye.proofread.entity.Post;
 import com.ziorye.proofread.entity.User;
 import com.ziorye.proofread.repository.PostRepository;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -43,6 +45,47 @@ class ResourceControllerTest {
         mvc.perform(MockMvcRequestBuilders
                         .get("/posts/" + post.getId()))
                 .andExpect(MockMvcResultMatchers.status().isOk())
+        ;
+
+        postRepository.deleteById(post.getId());
+    }
+
+    @Test
+    void downloadLoginRequired() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/resources/download/" + Long.MAX_VALUE))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrlPattern("**/login"))
+        ;
+    }
+
+    @Test
+    @WithMockUser
+    void downloadNonExistentResource() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/resources/download/" + Long.MAX_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+        ;
+    }
+
+    @Test
+    @WithMockUser
+    void download() throws Exception {
+        String resourceType = "resource";
+        Post post = new Post();
+        post.setCreated_at(LocalDateTime.now());
+        String title = UUID.randomUUID().toString();
+        post.setTitle(title);
+        post.setContent(UUID.randomUUID().toString());
+        post.setUser(new User(1L));
+        post.setType(resourceType);
+        post.setAttachment("/attachment/stock-data-visualization-template.zip");
+        postRepository.save(post);
+
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/resources/download/" + post.getId()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.header().stringValues("Content-Disposition", Matchers.contains("attachment;filename=resource-" + post.getId() + ".zip")))
         ;
 
         postRepository.deleteById(post.getId());
