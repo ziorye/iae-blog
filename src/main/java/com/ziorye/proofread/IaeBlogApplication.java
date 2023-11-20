3408efb27594c7a73498f8802f61e620dc5a4d48
ziorye <ziorye@gmail.com>
2023-11-20 17:58:36 +0800

    update artifactId

--- src/main/java/com/ziorye/proofread/IaeBlogApplication.java
+++ src/main/java/com/ziorye/proofread/IaeBlogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IaeProofreadApplication {
+public class IaeBlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IaeProofreadApplication.class, args);
+        SpringApplication.run(IaeBlogApplication.class, args);
     }
 
 }
